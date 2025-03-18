7e469378d1d5fe5a3f3f7a3b0557f1c4352e4d5f
Tu Nombre <calambasmadrid16@hotmail.com>
2025-03-18 17:37:54 -0500

    ejercicio4y5 realizado

--- Program.cs
+++ Program.cs
@@ -2,7 +2,9 @@
 
 // SolicitarYProcesarNumero(); 
 // SolicitarYProcesarDosNumeros();
-RaizOalCuadrado();
+// RaizOalCuadrado();
+// PerimetroCirculo();
+DiaDeLaSemana();
 
 static void SolicitarYProcesarNumero()
 {
@@ -67,6 +69,40 @@ static void RaizOalCuadrado()
         else
         {
             Console.WriteLine($"Resultado: {numero * numero}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Entrada no válida.");
+    }
+}
+static void PerimetroCirculo()
+{
+    Console.Write("Ingrese el radio de un círculo: ");
+    if (double.TryParse(Console.ReadLine(), out double radio))
+    {
+        double perimetro = 2 * Math.PI * radio;
+        Console.WriteLine($"Resultado: {perimetro:F2}");
+    }
+    else
+    {
+        Console.WriteLine("Entrada no válida.");
+    }
+}
+
+static void DiaDeLaSemana()
+{
+    Console.Write("Ingrese un número entre 1 y 7: ");
+    if (int.TryParse(Console.ReadLine(), out int dia))
+    {
+        switch (dia)
+        {
+            case 1: Console.WriteLine("Resultado: Lunes"); break;
+            case 2: Console.WriteLine("Resultado: Martes"); break;
+            case 3: Console.WriteLine("Resultado: Miércoles"); break;
+            case 4: Console.WriteLine("Resultado: Jueves"); break;
+            case 5: Console.WriteLine("Resultado: Viernes"); break;
+            default: Console.WriteLine("Resultado: Número fuera del rango laboral."); break;
         }
     }
     else

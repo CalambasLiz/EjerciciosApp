fe0f642e5aae8ffb87f67ad4bc7c74a818c62144
Tu Nombre <calambasmadrid16@hotmail.com>
2025-03-18 17:11:55 -0500

    ejercicio1 realizado

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+SolicitarYProcesarNumero();
+
+static void SolicitarYProcesarNumero()
+{
+    Console.Write("Ingrese un número: ");
+    if (double.TryParse(Console.ReadLine(), out double numero))
+    {
+        if (numero > 0)
+        {
+            Console.WriteLine($"Resultado: {numero * numero}");
+        }
+        else if (numero < 0)
+        {
+            Console.WriteLine("Resultado: Número negativo.");
+        }
+        else
+        {
+            Console.WriteLine("Resultado: 0");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Entrada no válida.");
+    }
+}
